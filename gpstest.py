c5d4dcb4e505f10493399fbd34f87953d3aeb1d9
Andrew Pham <andrew11798@gmail.com>
2019-11-16 13:59:51 -0800

    gps imu read code

--- gpstest.py
+++ gpstest.py
@@ -1,9 +1,12 @@
 import serial
-import datetime
+import time
+import pynmea2
 
 ser = serial.Serial("/dev/ttyACM0", 9600)
 
 while True:
-    print(ser.readline())
-
-    time.sleep(5)
+    msg = ser.readline().decode().strip('\r\n')
+    if (msg[0:6] == '$GNGGA'):
+        parsed_msg = pynmea2.parse(msg)
+        print(parsed_msg.lat)
+    time.sleep(1)
